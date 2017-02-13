wmcg-measure/snowplow

Cross-batch deduplication (WIP ticket name #2999)

3-enrich/scala-hadoop-shred/project/SnowplowHadoopShredBuild.scala
@@ -44,6 +44,7 @@ object SnowplowHadoopShredBuild extends Build {
         Libraries.commonEnrich,
         Libraries.scaldingCore,
         Libraries.scaldingArgs,
+        Libraries.awscala,
         // Libraries.scaldingJson,
         Libraries.scalaz7,
         Libraries.igluClient,

chuwy/snowplow-build-test

3-enrich/scala-hadoop-shred/project/SnowplowHadoopShredBuild.scala
@@ -39,6 +39,7 @@ object SnowplowHadoopShredBuild extends Build {
         Libraries.jsonValidator,
         Libraries.yodaTime,
         Libraries.yodaConvert,
+        Libraries.dynamodb,
         // Scala
         Libraries.json4sJackson,
         Libraries.commonEnrich,
e06dd3bbfea933961586e4b49660f58f44a67ca4
Anton Parkhomenko <mailbox@chuwy.me>
2016-12-22 17:19:22 +0700

    Cross-batch deduplication (WIP ticket name #2999)

--- 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/SpecHelpers.scala
+++ 3-enrich/scala-hadoop-shred/src/test/scala/com.snowplowanalytics.snowplow.enrich.hadoop/SpecHelpers.scala
@@ -18,6 +18,10 @@ package hadoop
 // Iglu Scala Client
 import iglu.client.Resolver
 
+// Scalaz
+import scalaz._
+import Scalaz._
+
 // This project
 import utils.JsonUtils
 
@@ -29,6 +33,7 @@ object SpecHelpers {
 
   // Internal
   private val igluConfigField = "IgluConfigField"
+  private val duplicateStorageField = "DuplicateStorageConfigField"
 
   /**
    * Standard Iglu configuration.
@@ -60,6 +65,58 @@ object SpecHelpers {
   val IgluResolver = (for {
     json <- JsonUtils.extractJson(igluConfigField, IgluConfig)
     reso <- Resolver.parse(json)
-  } yield reso).getOrElse(throw new RuntimeException("Could not build an Iglu resolver, should never happen")) 
+  } yield reso).getOrElse(throw new RuntimeException("Could not build an Iglu resolver, should never happen"))
 
+
+  /**
+    * Duplicate storage configuration, enabling cross-batch deduplication on CI environment
+    * If CI is set and all envvars are available it becomes valid schema
+    * If not all envvars are available, but CI is set - it will throw runtime exception as impoperly configured CI environment
+    * If not all envvars are available, but CI isn't set - it will return empty JSON, which should not be used anywhere (in JobSpecHelpers)
+    */
+  val DuplicateStorageConfig = getDynamodbConfig.map { case (accessKeyId, secretAccessKey, awsRegion, dynamodbTable) =>
+    s"""|{
+         |"schema": "iglu:com.snowplowanalytics.snowplow.storage/amazon_dynamodb_config/jsonschema/1-0-0",
+         |"data": {
+           |"name": "local",
+           |"accessKeyId": "$accessKeyId",
+           |"secretAccessKey": "$secretAccessKey",
+           |"awsRegion": "$awsRegion",
+           |"dynamodbTable": "$dynamodbTable",
+           |"purpose": "DUPLICATE_TRACKING"
+         |}
+       |}""".stripMargin
+  } match {
+    case Success(config) => config
+    case Failure(error) if CI => throw new RuntimeException("Cannot get AWS DynamoDB CI configuration. " + error.toList.mkString(", "))
+    case Failure(_) => "{}"
+  }
+
+  /**
+   * Check if tests are running in continous integration environment
+   */
+  def CI = sys.env.get("CI") match {
+    case Some("true") => true
+    case _ =>
+      println("WARNING! Test requires CI envvar to be set and DynamoDB local instance to be running")
+      false
+  }
+
+  /**
+   * Get environment variable wrapped into `Validation`
+   */
+  def getEnv(envvar: String): ValidationNel[String, String] = sys.env.get(envvar) match {
+    case Some(v) => Success(v)
+    case None => Failure(NonEmptyList(s"Environent variable [$envvar] is not available"))
+  }
+
+  /**
+   * Get environment varables required to access to DynamoDB
+   */
+  def getDynamodbConfig = {
+    val configValidation = getEnv("ACCESS_KEY_ID") |@| getEnv("SECRET_ACCESS_KEY") |@| getEnv("AWS_REGION") |@| getEnv("DYNAMODB_TABLE")
+    configValidation { (accessKeyId, secretAccessKey, awsRegion, dynamodbTable) =>
+      (accessKeyId, secretAccessKey, awsRegion, dynamodbTable)
+    }
+  }
 }
